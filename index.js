f61bb81758c8432c2a8443e77bb4f31b11734b53
Akira Taguchi <akirahattivatti@gmail.com>
2020-07-08 13:43:09 +0300

    3.3 done

--- index.js
+++ index.js
@@ -36,6 +36,17 @@ app.get('/info', (req, res) => {
         </div>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
